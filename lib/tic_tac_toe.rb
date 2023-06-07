dd7806c3afdc3772fe595e50d2d0471287ee099e
Staci Rosales <stacileep2@gmail.com>
2023-06-07 10:25:27 -0500

    commit tree 3e2b787061c5a468d355e1df9e8e81142e6af128

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,36 @@
+class TicTacToe
+ 
+# In other words, your `#initialize` method should set a `@board` variable equal
+# to a new, empty array that represents the game board.
+
+    def initialize
+        @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    end
+
+    WIN_COMBINATIONS = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ]
+
+    def display_board
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts "-----------"
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts "-----------"
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+
+    def input_to_index (user_input)
+        user_input.to_i - 1
+    end
+
+    def move (index, token)
+        @board[index] = token
+    end
+
+    
+end
+
+
+
